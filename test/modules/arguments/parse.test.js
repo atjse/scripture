1f717d525df5d57589f79f6f8cae320285af24ee
Richard Szakacs <richardszkcs@gmail.com>
2017-07-07 05:29:25 +0200

    Update arguments tests

--- test/modules/arguments/parse.test.js
+++ test/modules/arguments/parse.test.js
@@ -12,8 +12,8 @@ tap.test('parse invalid', t => {
   const args6 = Arguments.parse(1)
   const args7 = Arguments.parse(NaN)
 
-  t.equal(args1.numArguments(), 2)
-  t.equal(args1.hasArguments(), true)
+  t.equal(args1.numArguments(), 0)
+  t.equal(args1.hasArguments(), false)
 
   t.equal(args2.numArguments(), 0)
   t.equal(args2.hasArguments(), false)
@@ -40,27 +40,36 @@ tap.test('parse valid', t => {
   const args1 = Arguments.parse('')
   const args2 = Arguments.parse('a')
   const args3 = Arguments.parse('a b c')
-  const args4 = Arguments.parse([])
-  const args5 = Arguments.parse(['a'])
-  const args6 = Arguments.parse(['a', 'b', 'c'])
+  const args4 = Arguments.parse('a b c d e')
+
+  const args5 = Arguments.parse([])
+  const args6 = Arguments.parse(['a'])
+  const args7 = Arguments.parse(['a', 'b', 'c'])
+  const args8 = Arguments.parse(['a', 'b', 'c', 'd', 'e'])
 
   t.equal(args1.numArguments(), 0)
   t.equal(args1.hasArguments(), false)
 
-  t.equal(args2.numArguments(), 1)
-  t.equal(args2.hasArguments(), true)
+  t.equal(args2.numArguments(), 0)
+  t.equal(args2.hasArguments(), false)
 
-  t.equal(args3.numArguments(), 3)
+  t.equal(args3.numArguments(), 1)
   t.equal(args3.hasArguments(), true)
 
-  t.equal(args4.numArguments(), 0)
-  t.equal(args4.hasArguments(), false)
+  t.equal(args4.numArguments(), 3)
+  t.equal(args4.hasArguments(), true)
 
-  t.equal(args5.numArguments(), 1)
-  t.equal(args5.hasArguments(), true)
+  t.equal(args5.numArguments(), 0)
+  t.equal(args5.hasArguments(), false)
 
-  t.equal(args6.numArguments(), 3)
-  t.equal(args6.hasArguments(), true)
+  t.equal(args6.numArguments(), 0)
+  t.equal(args6.hasArguments(), false)
+
+  t.equal(args7.numArguments(), 1)
+  t.equal(args7.hasArguments(), true)
+
+  t.equal(args8.numArguments(), 3)
+  t.equal(args8.hasArguments(), true)
 
   t.end()
 })
